c82b4ce92f944e55ecd496e9438957404c375de3
Asif Mohammad Mollah <asif.mollah@ingreens.in>
2018-01-04 14:36:08 +0530

    php artisan jwt:generate

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'pEGalb8o9dwcwwa4dlBIUNwDkPhDni3b'),
 
     /*
     |--------------------------------------------------------------------------
